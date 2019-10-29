64f63d4a555d75ca3fd103133c7804819eafeb94
Jacob Walser <jwalser90@gmail.com>
2019-10-28 23:39:46 -0400

    main cleanup

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,12 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
 #include <register-model.h>
-bool lockkk;
 
 #include <QRandomGenerator>
 
 #include <qcustomplot.h>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -21,62 +21,17 @@ MainWindow::MainWindow(QWidget *parent)
         ui->label_3->setText(QString::number(value));
         device->setThrottle(value);
     });
+
     connect(&portScanner, &PortScanner::scanUpdate, this, &MainWindow::onPortScanFinished);
     portScanner.startScanning(500);
-//    ui->customPlot->setOpenGl(true);
 
     for (uint8_t i = 0; i < 20; i++) {
-
         ui->customPlot->addGraph()->setVisible(false);
-        ui->customPlot->graph(i)->setPen(QColor(QRandomGenerator::global()->bounded(0,255), QRandomGenerator::global()->bounded(0, 255), QRandomGenerator::global()->bounded(0,255)));
-
-    //    ui->customPlot->addGraph(0, ui->customPlot->yAxis2)->setVisible(false);
-
+        // random color for each plot
+        ui->customPlot->graph(i)->setPen(QColor(QRandomGenerator::global()->bounded(50,255), QRandomGenerator::global()->bounded(50, 255), QRandomGenerator::global()->bounded(50,255)));
     }
 
     ui->customPlot->yAxis2->setVisible(true);
-//    ui->customPlot->graph(0)->setPen(QColor(Qt::red));
-//    ui->customPlot->graph(1)->setPen(QColor(Qt::blue));
-//    ui->customPlot->graph(2)->setPen(QColor(Qt::cyan));
-//    ui->customPlot->graph(3)->setPen(QColor(Qt::green));
-//    ui->customPlot->graph(4)->setPen(QColor(Qt::black));
-//    ui->customPlot->graph(5)->setPen(QColor(Qt::magenta));
-//    ui->customPlot->graph(6)->setPen(QColor(Qt::darkRed));
-//    ui->customPlot->graph(7)->setPen(QColor(Qt::darkYellow));
-
-//    QStringList list;
-//    list << "1" << "fuck3" << "asdf";
-
-//    registerModel.setStringList(list);
-//    ui->tableView->setModel(&registerModel);
-
-
-//    registerList.append({0x01, "one", 100});
-//    registerList.append({0x02, "two", 4});
-//    registerList.append({0x03, "three", 6});
-//    ui->tableView->setModel(&registerModel);
-
-    lockkk = false;
-//    connect(&replotTimer, &QTimer::timeout, [&](){
-//        lockkk = true;
-//        static QTime time(QTime::currentTime());
-//        // calculate two new data points:
-//        double key = time.elapsed()/1000.0; // time elapsed since start of demo, in seconds
-//        ui->customPlot->xAxis->setRange(key, 5, Qt::AlignRight);
-
-//        ui->customPlot->graph(0)->rescaleValueAxis(false, true);
-//        ui->customPlot->graph(1)->rescaleValueAxis(true, true);
-//        ui->customPlot->graph(2)->rescaleValueAxis(true, true);
-//        ui->customPlot->graph(3)->rescaleValueAxis(true, true);
-//        ui->customPlot->graph(4)->rescaleValueAxis(false, true);
-//        ui->customPlot->graph(5)->rescaleValueAxis(true, true);
-//        ui->customPlot->replot(QCustomPlot::rpQueuedReplot);
-//    });
-
-
-//    CheckBoxItemDelegate* cbid = new CheckBoxItemDelegate(ui->tableView);
-//    // ComboBox only in column 2
-//    ui->tableView->setItemDelegateForColumn(3, cbid);
 }
 
 void MainWindow::onPlotEnabledChanged(int index) {
